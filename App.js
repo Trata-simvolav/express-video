6dffb86b009a4d2c134d60dbdead0c851f602271
Trata-simvolav <114504094+Trata-simvolav@users.noreply.github.com>
2024-05-28 21:12:48 +0500

    0.0.5

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// 0.0.4
+// 0.0.5
 
 // ---- ИМПОРТ ---- //
 const express = require('express');
--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -20,13 +20,21 @@ router.get('/test', (req, res) => {
 router.get('/:id/stream', videoController.startStream);
 
 // ПРИНЯТИЕ ВИДЕО С АДМИНКИ
-const upload = multer({ dest: path.join(__dirname, '../videos') }); // Используйте multer для обработки запросов с файлами
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../videos'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 router.post('/upload-video', upload.single('video'), async (req, res) => {
     try {
-        // // Получаем информацию о загруженном файле
         // const { filename, originalname, mimetype, size } = req.file;
-
-        // // Создаем новую запись о видео в базе данных
+        
         // const newVideo = new Video({
         //     filename,
         //     originalname,
@@ -34,18 +42,15 @@ router.post('/upload-video', upload.single('video'), async (req, res) => {
         //     size
         // });
 
-        // // Сохраняем запись в базе данных
         // await newVideo.save();
-
         console.log(req.file);
 
-        // Отправляем ответ клиенту
         res.status(201).send('Видео успешно загружено.');
-        console.log('VIDEO IMPORT ROUTE');
     } catch (error) {
         console.error('Ошибка при загрузке видео:', error);
         res.status(500).send('Произошла ошибка при загрузке видео.');
     }
+    console.log('VIDEO IMPORT ROUTE');
 });
 
 
